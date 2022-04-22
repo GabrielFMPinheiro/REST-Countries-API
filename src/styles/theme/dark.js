fa9d9c78c7366f2cb3f24c1f64b034f34757e2c7
Gabriel <g.felippe5965@gmail.com>
2022-04-22 19:49:36 -0300

    feat(setup): creating the redux store and folder structure for the project

---
+++ src/styles/theme/dark.js
@@ -0,0 +1,11 @@
+export default {
+    title: 'dark',
+    background: {
+        primary: '#202C36',
+        secundary: '#2B3844',
+    },
+    'text-colors': {
+        primary: '#FFFFFF',
+        secundary: '#FFFFFF',
+    },
+};
---
+++ src/styles/theme/light.js
@@ -0,0 +1,11 @@
+export default {
+    title: 'light',
+    background: {
+        primary: '#F2F2F2',
+        secundary: '#FFFFFF',
+    },
+    'text-colors': {
+        primary: '#848484',
+        secundary: '#111517',
+    },
+};
